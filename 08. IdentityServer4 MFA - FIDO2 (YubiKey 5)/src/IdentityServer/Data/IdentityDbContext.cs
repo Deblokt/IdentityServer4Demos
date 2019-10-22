781727ad5dee875065b63a59afbdbd548d4cc8d5
Ivan <ivansed_scg@hotmail.com>
2019-10-22 11:48:49 +0200

    Added FIDO2 tutorial

---
+++ 08. IdentityServer4 MFA - FIDO2 (YubiKey 5)/src/IdentityServer/Data/IdentityDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityServer.Models
+{
+    public class IdentityDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public IdentityDbContext(DbContextOptions<IdentityDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<FidoStoredCredential> FidoStoredCredential { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<FidoStoredCredential>().HasKey(m => m.Username);
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ 08. IdentityServer4 MFA - FIDO2 (YubiKey 5)/src/IdentityServer/Program.cs
@@ -0,0 +1,53 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer.Data.Seed;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.SystemConsole.Themes;
+using System;
+
+namespace IdentityServer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Title = "IdentityServer4";
+
+            var host = CreateWebHostBuilder(args).Build();
+
+            var config = host.Services.GetRequiredService<IConfiguration>();
+            bool seed = config.GetSection("Data").GetValue<bool>("Seed");
+            if (seed)
+            {
+                var connectionString = config.GetConnectionString("DefaultConnection");
+                Users.EnsureSeedData(connectionString);
+            }
+
+            host.Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            return WebHost.CreateDefaultBuilder(args)
+                    .UseStartup<Startup>()
+                    .UseSerilog((context, configuration) =>
+                    {
+                        configuration
+                            .MinimumLevel.Debug()
+                            .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                            .MinimumLevel.Override("System", LogEventLevel.Warning)
+                            .MinimumLevel.Override("Microsoft.AspNetCore.Authentication", LogEventLevel.Information)
+                            .Enrich.FromLogContext()
+                            .WriteTo.File(@"identityserver4_log.txt")
+                            .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}", theme: AnsiConsoleTheme.Literate);
+                    });
+        }
+    }
+}
